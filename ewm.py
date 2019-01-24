22678adc1a823ad0376a43405ccd8f3fcf62b7b4
hzphzp <huangzp1104@gmail.com>
2019-01-24 19:33:20 +0800

    sss

--- ewm.py
+++ ewm.py
@@ -1,13 +1,9 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Nov  7 11:08:54 2016
 
-@author: debin Lu
-"""
 import numpy as np
 import pandas as pd
-fp = "d:/shangquan.xlsx"
-data = pd.read_excel(fp, index_col=None, header=None, encoding='utf8')
+fp = "d:/code/tmp/data1.csv"
+data = pd.read_csv(fp, index_col=None, header=None, encoding='utf8')
 # data = (data - data.min())/(data.max() - data.min())
 m, n = data.shape
 # 第一步读取文件，如果未标准化，则标准化
@@ -23,3 +19,4 @@ ej = -k*(test.sum(axis=0))
 # 计算每种指标的信息熵
 wi = (1-ej)/np.sum(1-ej)
 # 计算每种指标的权重
+print(wi)
